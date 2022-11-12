abd6c2e358223aef19b7d7ccf4354e487a9bef90
Benedict <newlyf38@gmail.com>
2022-11-12 02:59:08 +0000

    created a alltodo view in the view.py

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import MyToDo
 # Register your models here.
+
+admin.site.register(MyToDo)
--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import MyToDo
 # Create your views here.
 def alltodo(request):
+    
+    tasks = MyToDo.objects.all()
+    context = {
+        'tasks':tasks,
+    }
     return render(request, 'myApp/alltodo', context)
