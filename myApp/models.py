f0025d775d408c33232f3c964a86e567065dcd72
Benedict <newlyf38@gmail.com>
2022-11-12 02:56:46 +0000

    created a MyToDo model in models.py

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class MyToDo(models.Model):
+    task = models.CharField(max_length=100, unique=True)
+    
+    def _str__(self):
+        return self.task
--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def alltodo(request):
+    return render(request, 'myApp/alltodo', context)
