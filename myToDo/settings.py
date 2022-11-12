396495007b6b4a10d36ff532e6a8dd66650aa2ae
Benedict <newlyf38@gmail.com>
2022-11-12 02:50:02 +0000

    started a project called myToDo

--- myToDo/settings.py
+++ myToDo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x3-&hhf@=+=qzow2=83gvz@7z99%lg^vcprp!yq^cdrg06_no5'
+SECRET_KEY = 'django-insecure-95)_n$42v-o)+maxyqgwusaz&vuuyhkc0t)j@8flw6+n%hfz(-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
